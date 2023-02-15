623319c524074de8c5ddd60135258275ad30c249
nagy <nagyy8751@gmail.com>
2023-02-15 20:47:06 +0200

    for get users correct

--- controllers/user.js
+++ controllers/user.js
@@ -65,7 +65,7 @@ const handleLogin = async (req, res) => {
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.find({})
-    res.send(200).json(users)
+    res.status(200).send(users)
   } catch (error) {
     res.status(400).json({
       message: error.message,
